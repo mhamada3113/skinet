0e5b329b0025bfcaf52cac243d6a635babfdabb1
Mohamed Hamada <MohamedH@Omicron.com>
2020-10-20 11:40:37 +0300

    End of section 6-1

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("api/[controller]")] //means that the route will be api/BaseApi --> api/Products because Products controller is inhertence from BaseApi
     public class BaseApiController : ControllerBase
     {
         
